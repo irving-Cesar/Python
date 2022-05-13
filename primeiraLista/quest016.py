b3903df8d7c87acbc9c9e2255f2d76a9de3cd2f7
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-12 21:48:57 -0300

    Create quest016.py

---
+++ primeiraLista/quest016.py
@@ -0,0 +1,17 @@
+area = float(input('Insira o tamanho da área: '))
+
+litro_por_metro = 3
+lata_litro = 18
+lata_preco = 80
+
+lata_comprada = area / lata_litro
+lata_comprada = int(lata_comprada)
+
+if not (area % lata_litro) == 0:
+    lata_comprada+=1
+
+calc_litro_usado = area / litro_por_metro
+
+valor_total = lata_comprada * lata_preco
+
+print('Valor a ser pago: {:.2f} R$, quantiadade de latas compradas {}.'.format(valor_total, int(lata_comprada)))
