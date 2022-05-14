0fba32541e571495ea25fb3d6c2f386811ab54a0
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-14 14:37:44 -0300

    Create quest009.py

---
+++ segundLista/quest009.py
@@ -0,0 +1,10 @@
+arr_dec = []
+for n in range(1, 4):
+    num = int(input('Insira o {}° preço: '.format(n)))
+    
+    arr_dec.append(num)
+
+arr_dec.sort()
+arr_dec = list(reversed(arr_dec))
+
+print(arr_dec)
