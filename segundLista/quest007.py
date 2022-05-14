e9ca8160607d563f8895670f8938c7569b1fea76
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-14 14:23:44 -0300

    Create quest007.py

---
+++ segundLista/quest007.py
@@ -0,0 +1,13 @@
+for n in range(1, 4):
+    num = int(input('Insira o {}° numero: '.format(n)))
+    if n == 1:
+        maior = num
+        menor = num
+        
+    if num > maior:
+        maior = num
+    
+    if num < menor:
+        menor = num
+    
+print('Maior {} e menor {}'.format(maior, menor))
