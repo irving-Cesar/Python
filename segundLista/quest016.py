afcecfed79551d486b789cc9179136aec0f936f3
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-15 17:21:28 -0300

    Update quest016.py

--- segundLista/quest016.py
+++ segundLista/quest016.py
@@ -1,12 +1,8 @@
-# formula = 'valor_a * x**2 + valor_b * x + valor_c'
-
 valor_a = int(input('Insira o valor de A: '))
 if valor_a != 0:
     valor_b = int(input('Insira o valor de B: '))
     valor_c = int(input('Insira o valor de C: '))
-    
-    valor_a = valor_a ** 2
-    print(valor_a)
+
     calc_delta = valor_b**2 - 4 * valor_a * valor_c
 
     if calc_delta < 0:
