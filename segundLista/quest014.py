8511fc82dd7f2529a862c8b60437ae2fd253d0b9
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-15 16:35:33 -0300

    Update quest014.py

--- segundLista/quest014.py
+++ segundLista/quest014.py
@@ -5,13 +5,19 @@ for n in range(1, 3):
     
 media = sum(arr_nota) / len(arr_nota)
 
+print('Sua media foi: {:.1f}'.format(media))
 if media >= 9 and media <= 10:
     print('Nota: A')
+    print('Aprovado!')
 elif media >= 7.5 and media < 9:
     print('Nota: B')
+    print('Aprovado!')
 elif media >= 6 and media < 7.5:
     print('Nota: C')
+    print('Aprovado!')
 elif media >= 4 and media < 6:
     print('Nota: D')
+    print('Reprovado!')
 elif media >= 0 and media < 4:
     print('Nota: E')
+    print('Reprovado!')
