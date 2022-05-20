564f7d30e4a3ede1e4ce23f388a1793726e27e2d
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-20 02:07:40 -0300

    Update quest019.py

--- segundLista/quest019.py
+++ segundLista/quest019.py
@@ -1,10 +1,12 @@
+#  326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
+
 num = str(input('Insira um número inteiro menor que 1000: '))
 tamanho_max = 3;
 val_string = []
 str_set = ''
 arr_tipos = ['centenas', 'dezenas', 'unidades']
 
-if int(num) < 1000 and num.isnumeric():
+if int(num) < 1000 and num.isnumeric() and int(num) != 0:
     
     origin_len =len(num)    
     # inserir zero onde não há número. ex: 12 -> 012; 1 -> 001
@@ -23,6 +25,7 @@ if int(num) < 1000 and num.isnumeric():
                 arr_tipos[num.index(num[i+1])] = arr_tipos[num.index(num[i+1])].replace('s', '')
             str_set = (f'{num[i+1]} {arr_tipos[num.index(num[i+1])]}')
             val_string.append(str_set)
+            num = num.replace(num[num.index(num[i+1])], 'x', 1)
         print(' e '.join(val_string))
         
     elif origin_len == 3:
@@ -31,7 +34,7 @@ if int(num) < 1000 and num.isnumeric():
                 arr_tipos[num.index(num[i])] = arr_tipos[num.index(num[i])].replace('s', '')
             str_set = (f'{num[i]} {arr_tipos[num.index(num[i])]}')
             val_string.append(str_set)
-            
+            num = num.replace(num[num.index(num[i])], 'x', 1)
         str_set = ''
         for x, y in zip(val_string, range(len(val_string))):
             if y < 2:
@@ -44,4 +47,4 @@ if int(num) < 1000 and num.isnumeric():
         print(str_set)
         
 else:
-    print('Não é um número ou é maior/igual a 1000.')
+    print('Não é um número ou é maior que 1000 ou igual a 0.')
