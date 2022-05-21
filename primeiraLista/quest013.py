863ec6e3dbcf808de66409571d6166a2421de617
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:03:36 -0300

    Update quest013.py

--- primeiraLista/quest013.py
+++ primeiraLista/quest013.py
@@ -1,3 +1,8 @@
+'''
+Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+a) Para homens: (72.7*h) - 58
+b) Para mulheres: (62.1*h) - 44.7
+'''
 alt = float(input('Insira a sua altura: '))
 imcH = (72.7 * alt) - 58
 imcM = (62.1 * alt) - 44.7
