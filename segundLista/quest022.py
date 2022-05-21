e4a09d370ca00d799668ec20e7f36ab0fb9e9cf2
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:29:46 -0300

    Update quest022.py

--- segundLista/quest022.py
+++ segundLista/quest022.py
@@ -1,4 +1,6 @@
-# Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+'''
+Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+'''
 
 num = int(input('Insira um número: '))
 print('ÍMPAR' if num % 2 == 1 else 'PAR')
