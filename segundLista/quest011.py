b3898708e09c9e6559d2d9733219e0f4f0e6c2dc
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:19:11 -0300

    Update quest011.py

--- segundLista/quest011.py
+++ segundLista/quest011.py
@@ -1,3 +1,15 @@
+'''
+As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para desenvolver o programa que calculará os reajustes.
+Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+    1- salários até R$ 280,00 (incluindo) : aumento de 20%
+    2- salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+    3- salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+    4- salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+    5- o salário antes do reajuste;
+    6- o percentual de aumento aplicado;
+    7- o valor do aumento;
+    8- o novo salário, após o aumento.
+'''
 salario_sem_reajuste = float(input('Insira o seu salário atual: '))
 
 salario_reajustado = 0
