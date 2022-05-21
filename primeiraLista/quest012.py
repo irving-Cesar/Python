7748e94c3845f8f39ebec06f914e25f10ddedb78
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:03:11 -0300

    Update quest012.py

--- primeiraLista/quest012.py
+++ primeiraLista/quest012.py
@@ -1,3 +1,6 @@
+'''
+Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+'''
 alt = float(input('Insira a sua altura: '))
 imc = (72.7 * alt) - 58
 print('Seu imc é: {}'.format(imc))
