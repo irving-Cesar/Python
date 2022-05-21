331f1418890a5dec6a7359c2e0a330b7e73ffeb5
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:12:32 -0300

    Create quest018.py

---
+++ primeiraLista/quest018.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+'''
+tamanho = float(input('Insira o tamanho do arquivo (Mbps): '))
+velo_internet_mpbs = 5
+calc = tamanho / velo_internet_mpbs
+
+print(f'terminará em {calc} minutos')
