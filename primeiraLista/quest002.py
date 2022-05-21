f0dc21359382e60584c3ccc72e79e7a98e3af97c
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-20 23:58:53 -0300

    Update quest002.py

--- primeiraLista/quest002.py
+++ primeiraLista/quest002.py
@@ -1,2 +1,6 @@
+'''
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+'''
+
 num = int(input('Insira um número: '))
 print('O número informado foi: {}'.format(num))
