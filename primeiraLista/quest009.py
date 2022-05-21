df6c6faef7dc089cb753b66bba1898473c6d507c
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:01:52 -0300

    Update quest009.py

--- primeiraLista/quest009.py
+++ primeiraLista/quest009.py
@@ -1,3 +1,7 @@
+'''
+Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+C = 5 * ((F-32) / 9).
+'''
 f = float(input('Insira o grau em Fahrenheit: '))
 c = 5 * ((f-32) / 9)
 print('Valor em Celsius: {:.1f}°'.format(c))
