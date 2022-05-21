23c73d1ea89310c77ed0259d180670f7b15295d3
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:00:54 -0300

    Update quest007.py

--- primeiraLista/quest007.py
+++ primeiraLista/quest007.py
@@ -1,3 +1,7 @@
+'''
+Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+'''
+
 l1 = float(input('Insira o valor do lado a: '))
 l2 = float(input('Insira o valor do lado b: '))
 print('Área: {}'.format((l1 * l2)))
