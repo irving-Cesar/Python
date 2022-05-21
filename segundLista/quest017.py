41c56f56863e236e2e9add36888bbd0d675c5a24
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:24:21 -0300

    Update quest017.py

--- segundLista/quest017.py
+++ segundLista/quest017.py
@@ -1,3 +1,7 @@
+'''
+Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
+'''
+
 ano = int(input('Insira ano que desejar: '))
 
 calc4 = ano % 4
