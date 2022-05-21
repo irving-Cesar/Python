e647758a8a6795cb73ce22958cb94cc08e477b17
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:21:31 -0300

    Update quest013.py

--- segundLista/quest013.py
+++ segundLista/quest013.py
@@ -1,3 +1,6 @@
+'''
+Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+'''
 arr_semanas = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
 for d in range(len(arr_semanas)):
