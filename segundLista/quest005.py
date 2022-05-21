47f763a6c2ac67050c010cd53d4a33c91d69681d
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:14:46 -0300

    Update quest005.py

--- segundLista/quest005.py
+++ segundLista/quest005.py
@@ -1,3 +1,9 @@
+'''
+Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+a) A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+b) A mensagem "Reprovado", se a média for menor do que sete;
+c) A mensagem "Aprovado com Distinção", se a média for igual a dez.
+'''
 nota1 = float(input('Primeira nota: '))
 nota2 = float(input('Segunda nota: '))
 calc_media = (nota1 + nota2) / 2
