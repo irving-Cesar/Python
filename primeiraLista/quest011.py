3bdc20e2b93f4b0b1bf41c9ac9530482bc5babb0
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:02:50 -0300

    Update quest011.py

--- primeiraLista/quest011.py
+++ primeiraLista/quest011.py
@@ -1,3 +1,10 @@
+'''
+Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+a) o produto do dobro do primeiro com metade do segundo.
+b) a soma do triplo do primeiro com o terceiro.
+c) o terceiro elevado ao cubo.
+'''
+
 numInt1 = int(input('Insira o primeiro número inteiro: '))
 numInt2 = int(input('Insira o segundo número inteiro: '))
 numFloat = float(input('Insira o número real: '))
