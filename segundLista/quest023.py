3816ba44f3cc90623c945695e4e2d185784f757a
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:30:10 -0300

    Update quest023.py

--- segundLista/quest023.py
+++ segundLista/quest023.py
@@ -1,4 +1,6 @@
-# Faça um Programa que peça um número e informe se o número é inteiro ou decimal.
+'''
+Faça um Programa que peça um número e informe se o número é inteiro ou decimal.
+'''
 
 num = float(input('Insira um número: '))
 
