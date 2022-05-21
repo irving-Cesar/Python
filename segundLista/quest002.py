6a43f91251fe4129d345819f770f62bacfe74352
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:13:27 -0300

    Update quest002.py

--- segundLista/quest002.py
+++ segundLista/quest002.py
@@ -1,3 +1,6 @@
+'''
+Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+'''
 num = float(input('Insira o número: '))
 
 print('Negativo ' if num < 0 else 'Positivo')
