a9648636a32823d54f1a18bc330f52a86ca8d97d
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-21 00:01:21 -0300

    Update quest008.py

--- primeiraLista/quest008.py
+++ primeiraLista/quest008.py
@@ -1,3 +1,7 @@
+'''
+Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+'''
+
 ht = float(input('Informe as horas trabalhadas durante o mês: '))
 qph = float(input('Quanto você recebe por hora: '))
 print('Salário: {:.2f} R$.'.format(ht * qph))
