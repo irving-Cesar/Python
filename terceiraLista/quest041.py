a47f97fcf1fcd0e177ab9780de8d788134662a66
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-27 18:33:55 -0300

    Update quest041.py

--- terceiraLista/quest041.py
+++ terceiraLista/quest041.py
@@ -18,24 +18,27 @@ R$ 1.100,00     100             3                       R$    366,00
 R$ 1.150,00     150             6                       R$    191,67
 '''
 
-valor_divida = float(input('Insira o valor da dívida: '))
+valor_divida = 1000
 divida_val = valor_divida
 valor_juros = 0
 valor_parcela = 0
 juros = 0
 arr_parcelas = [1, 3, 6, 9, 12]
-fmt = ''
+
 print('Valor da dívida - Valor dos Juro - Quantidade de parcelas - Valor da Parcela')
 for i in arr_parcelas:
     if i == 3:
         juros += 0.1
     elif i > 3:
         juros += 0.05
-        
+
     valor_juros = (divida_val * juros)
     valor_divida = divida_val + valor_juros
-    
-    print(f'\nR$ {valor_divida:.2f}{fmt: ^15}R$ {valor_juros:.2f}{fmt: ^15}{i}{fmt: ^15}R$ {valor_divida / i:.2f}')
+
+    print(f'\nR$ {valor_divida:<10.2f}', end='')
+    print(f'       R$ {valor_juros:<10.2f}', end='')
+    print(f'{i:^24}',end='')
+    print(f'     R${valor_divida / i:^13.2f}')
     
 
     
