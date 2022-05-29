f86bd35ac680d5c00402d95efa27effc1bb60917
Irving Cesar <60630262+irving-Cesar@users.noreply.github.com>
2022-05-28 23:40:12 -0300

    Update quest049.py

--- terceiraLista/quest049.py
+++ terceiraLista/quest049.py
@@ -1,14 +1,17 @@
 '''
 Faça um programa que mostre os n termos da Série a seguir:
   S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m. 
+  Imprima no final a soma da série.
 '''
 cont_n = int(input('Quantidade: '))
 result = 'S = '
+soma = 0
 for n, m in zip(range(1, cont_n+1), range(1, cont_n*2, 2)):
     result += f'{n}/{m}'
     if n < cont_n:
         result += ' + '
     else:
         result += '.'
-    
-print(result)
+    soma += n/m
+print(result,end='')
+print(f' = {soma:.2f}')
